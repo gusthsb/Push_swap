2438ad9fc220f0b88c2283af14de7c3416f1a92b
gusthsb <gustavo.hdsbrito@gmail.com>
2026-07-01 23:44:01 -0300

    push func index

--- push_swap/def_index.c
+++ push_swap/def_index.c
@@ -1,7 +1,7 @@
 #include "ft_push_swap.h"
 
 static int	lst_size(t_list *lst) /*não precisa de ponteiro duplo quando a função só faz a leitura da lst*/
-{
+{								  /* *lst faz uma copia do ENDEREÇO de memoria, content continua sendo o original */
 	int	size;
 
 	size = 0;
@@ -11,4 +11,25 @@ static int	lst_size(t_list *lst) /*não precisa de ponteiro duplo quando a funç
 		lst = lst -> next;
 	}
 	return (size);
+}
+
+static void	sort_array(int *array, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i;
+		while (j > 0 && array[j - 1] > array[j])
+		{
+			temp = array[j - 1];
+			array[j - 1] = array[j];
+			array[j] = temp;
+			j--;
+		}
+		i++;
+	}
 }
