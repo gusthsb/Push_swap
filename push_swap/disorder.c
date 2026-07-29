0a9728219a18a2151a81de68bd731f9f3f1f6b95
gusthsb <gustavo.hdsbrito@gmail.com>
2026-07-29 13:24:37 -0300

    test disorder.c

--- push_swap/disorder.c
+++ push_swap/disorder.c
@@ -1,19 +1,19 @@
 #include "ft_push_swap.h"
 
-static int	*trans(t_list **lst_en, int *size) // precisa ser um array de int
+static int	*trans(t_list *lst_en, int *size) // precisa ser um array de int
 {
 	int		*new_lst; // mesma coisa
 	int		i;
 	t_list	*current;
 
-	if (!(*lst_en) || !lst_en)
+	if (!lst_en)
 		return (NULL);
 	*size = lst_size(lst_en); 
 	new_lst = malloc(sizeof(int) * (*size));
 	if (!new_lst)
 		return (NULL);
 	i = 0;
-	current = *lst_en;
+	current = lst_en;
 	while (i < *size)
 	{
 		new_lst[i] = current -> content;
@@ -42,7 +42,9 @@ double	calculate_disorder(int *lst_a, int size)
 			total_pairs++;
 			if (lst_a[i] > lst_a[j])
 				mistakes++;
+			j++;
 		}
+		i++;
 	}
 	if (total_pairs == 0)
 		return (0.0);
@@ -62,4 +64,30 @@ double main_disorder(t_list *lst)
 	free(new_arr);
 	return (value);
 }
+// teste disordem / lst_size
+#include <stdio.h>
+int	main(void)
+{
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
 
+	node1 = malloc(sizeof(t_list));
+	node1 -> content = 4;
+	node2 = malloc(sizeof(t_list));
+	node2 -> content = 1;
+	node3 = malloc(sizeof(t_list));
+	node3 -> content = 3;
+	
+	node1 -> next = node2;
+	node2 -> next = node3;
+	node3 -> next = NULL;
+
+	printf("Tamanho da lst = %d\n", lst_size(node1));
+	printf("Desordem = %f\n", main_disorder(node1));
+
+	free(node1);
+	free(node2);
+	free(node3);
+	return (0);
+}
