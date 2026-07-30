3f560f820e662c1d3c4cd62adc6c3dbdefb1509d
gusthsb <gustavo.hdsbrito@gmail.com>
2026-07-30 15:58:53 -0300

    parsing fix

--- push_swap/parsing.c
+++ push_swap/parsing.c
@@ -1,15 +1,33 @@
 #include "ft_push_swap.h"
 /* Verificar se o valor está duplicado*/
-int	is_arr_duplicate(t_list *lst_a, int value)
+// int	is_arr_duplicate(t_list *lst_a, int value)
+// {
+// 	t_list	*current;
+
+// 	current = lst_a;
+// 	while (current)
+// 	{
+// 		if (current -> content == value)
+// 			return (1);
+// 		current = current ->  next;
+// 	}
+// 	return (0);
+// }
+
+int	is_arr_duplicate(t_list *lst, int value)
 {
 	t_list	*current;
 
-	current = lst_a;
-	while (current)
+	if (!lst)
+		return (0);
+	if (lst -> content == value)
+		return (1);
+	current = lst -> next;
+	while (current != lst)
 	{
 		if (current -> content == value)
 			return (1);
-		current = current ->  next;
+		current = current -> next;
 	}
 	return (0);
 }
