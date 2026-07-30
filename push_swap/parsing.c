09cd9a93322d3a3103387aed49d161973aaab7eb
gusthsb <gustavo.hdsbrito@gmail.com>
2026-07-30 10:14:26 -0300

    feat michaeljackson

--- push_swap/parsing.c
+++ push_swap/parsing.c
@@ -1,3 +1,15 @@
 #include "ft_push_swap.h"
 
+int	is_arr_duplicate(t_list *lst_a, int value)
+{
+	t_list	*current;
 
+	current = lst_a;
+	while (current)
+	{
+		if (current -> content == value)
+			return (1);
+		current = current ->  next;
+	}
+	return (0);
+}
