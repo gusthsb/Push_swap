dfc12e7ef1d0b9cafbdf99eb1865499c6b67afbb
gusthsb <gustavo.hdsbrito@gmail.com>
2026-08-05 22:43:06 -0300

    modificando reverse/write op

--- push_swap/reverse.c
+++ push_swap/reverse.c
@@ -12,22 +12,28 @@
 
 #include "ft_push_swap.h"
 
+static void	reverse(t_list **lst)
+{
+	if (!lst || !*lst || (*lst)->next == *lst)
+		return ;
+	*lst = (*lst)->prev;
+}
+
 void	reverse_rotate_a(t_list **lst_a)
 {
-	if (!lst_a || !(*lst_a) || !(*lst_a) -> next)
-		return ;
-	*lst_a = (*lst_a) -> prev;
+	reverse(lst_a);
+	write(1, "rra\n", 4);
 }
 
 void	reverse_rotate_b(t_list **lst_b)
 {
-	if (!lst_b || !(*lst_b) || !(*lst_b) -> next)
-		return ;
-	*lst_b = (*lst_b) -> prev;
+	reverse(lst_b);
+	write(1, "rrb\n", 4);
 }
 
 void	rrr(t_list **lst_a, t_list **lst_b)
 {
-	reverse_rotate_a(lst_a);
-	reverse_rotate_b(lst_b);
+	reverse(lst_a);
+	reverse(lst_b);
+	write(1, "rrr\n", 4);
 }
