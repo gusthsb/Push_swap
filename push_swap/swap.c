22a1ee74a75965255fd0e5f55cb32a01b3471a75
gusthsb <gustavo.hdsbrito@gmail.com>
2026-08-05 22:37:59 -0300

    modificando swap/write op

--- push_swap/swap.c
+++ push_swap/swap.c
@@ -1,59 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gustde-s <gustde-s@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/30 17:11:37 by gustde-s          #+#    #+#             */
-/*   Updated: 2026/07/28 16:02:30 by gustde-s         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_push_swap.h"
 
-void	swap(t_list **lst)
+static void	do_swap(t_list **lst)
 {
 	t_list	*first;
 	t_list	*second;
 	t_list	*before;
 	t_list	*after;
 
-	if (!lst || !(*lst) || (*lst) -> next == *lst)
-		return ;
 	first = *lst;
-	second = first -> next;
-	if (second -> next == first)
-	{
-		*lst = second;
-		return ;
-	}
-	before = first -> prev;
-	after = second -> next;
-	before -> next = second;
-	second -> prev = before;
-	second -> next = first;
-	first -> prev = second;
-	first -> next = after;
-	after -> prev = first;
+	second = first->next;
+	before = first->prev;
+	after = second->next;
+	before->next = second;
+	second->prev = before;
+	second->next = first;
+	first->prev = second;
+	first->next = after;
+	after->prev = first;
 	*lst = second;
 }
 
 void	swap_a(t_list **lst_a)
 {
-	swap(lst_a);
+	if (!lst_a || !*lst_a || (*lst_a)->next == *lst_a)
+		return ;
+	if ((*lst_a)->next->next == *lst_a)
+		*lst_a = (*lst_a)->next;
+	else
+		do_swap(lst_a);
 	write(1, "sa\n", 3);
 }
 
-void	swap_a(t_list **lst_b)
+void	swap_b(t_list **lst_b)
 {
-	swap(lst_b);
+	if (!lst_b || !*lst_b || (*lst_b)->next == *lst_b)
+		return ;
+	if ((*lst_b)->next->next == *lst_b)
+		*lst_b = (*lst_b)->next;
+	else
+		do_swap(lst_b);
 	write(1, "sb\n", 3);
 }
 
-void swap_both(t_list **lst_a, t_list **lst_b)
+void	swap_both(t_list **lst_a, t_list **lst_b)
 {
-	swap(lst_a);
-	swap(lst_b);
+	if (lst_a && *lst_a && (*lst_a) -> next != *lst_a)
+	{
+		if ((*lst_a) -> next->next == *lst_a)
+			*lst_a = (*lst_a)->next;
+		else
+			do_swap(lst_a);
+	}
+	if (lst_b && *lst_b && (*lst_b)->next != *lst_b)
+	{
+		if ((*lst_b)->next->next == *lst_b)
+			*lst_b = (*lst_b)->next;
+		else
+			do_swap(lst_b);
+	}
 	write(1, "ss\n", 3);
 }
