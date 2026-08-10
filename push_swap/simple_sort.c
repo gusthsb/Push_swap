32a9802521d4dff5df6412ad0f680f2fcaa94860
Gustavo Henrique De Souza Brito <gustde-s@c3r11p5.42sp.org.br>
2026-08-10 16:38:17 -0300

    parametros/op count

--- push_swap/simple_sort.c
+++ push_swap/simple_sort.c
@@ -1,23 +1,23 @@
 #include "ft_push_swap.h"
 
-void	sort_simple(t_list **stack_a, t_list **stack_b)
+void	sort_simple(t_list **stack_a, t_list **stack_b, op_count *op)
 {
 	int	size;
 
 	size = lst_size(*stack_a);
 	if (size == 2)
-		two_sorted(stack_a);
+		two_sorted(stack_a, op);
 	else if (size == 3)
-		three_sorted(stack_a);
+		three_sorted(stack_a, op);
 	else
 	{
 		while (size > 3)
 		{
-			min_to_b(stack_a, stack_b);
+			min_to_b(stack_a, stack_b, op);
 			size--;
 		}
-		three_sorted(stack_a);
+		three_sorted(stack_a, op);
 		while (*stack_b)
-			push_a(stack_a, stack_b);
+			push_a(stack_a, stack_b, op);
 	}
 }
