cf24c7e2f3d6581e0bdc98f90caf19b149a87a37
Gustavo Henrique De Souza Brito <gustde-s@c3r11p5.42sp.org.br>
2026-08-10 16:17:40 -0300

    reverse op count/norm free

--- push_swap/reverse.c
+++ push_swap/reverse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   reverse.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamatos- <mamatos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustde-s <gustde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 20:22:36 by gustde-s          #+#    #+#             */
-/*   Updated: 2026/07/30 17:54:47 by mamatos-         ###   ########.fr       */
+/*   Updated: 2026/08/10 16:17:12 by gustde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,36 @@ static void	reverse(t_list **lst)
 	*lst = (*lst)->prev;
 }
 
-void	reverse_rotate_a(t_list **lst_a)
+void	reverse_rotate_a(t_list **lst_a, op_count *op)
 {
 	reverse(lst_a);
 	write(1, "rra\n", 4);
+	if (op)
+	{
+		op->rra++;
+		op->count++;
+	}
 }
 
-void	reverse_rotate_b(t_list **lst_b)
+void	reverse_rotate_b(t_list **lst_b, op_count *op)
 {
 	reverse(lst_b);
 	write(1, "rrb\n", 4);
+	if (op)
+	{
+		op->rrb++;
+		op->count++;
+	}
 }
 
-void	rrr(t_list **lst_a, t_list **lst_b)
+void	rrr(t_list **lst_a, t_list **lst_b, op_count *op)
 {
 	reverse(lst_a);
 	reverse(lst_b);
 	write(1, "rrr\n", 4);
+	if (op)
+	{
+		op->rrr++;
+		op->count++;
+	}
 }
