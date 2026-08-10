90c2b800f77efd364d7a13054e6a4923984b17b7
Gustavo Henrique De Souza Brito <gustde-s@c3r11p5.42sp.org.br>
2026-08-10 16:14:48 -0300

    swap op count/norm free

--- push_swap/swap.c
+++ push_swap/swap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustde-s <gustde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 16:12:54 by gustde-s          #+#    #+#             */
+/*   Updated: 2026/08/10 16:14:22 by gustde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_push_swap.h"
 
 static void	do_swap(t_list **lst)
@@ -20,7 +32,7 @@ static void	do_swap(t_list **lst)
 	*lst = second;
 }
 
-void	swap_a(t_list **lst_a)
+void	swap_a(t_list **lst_a, op_count *op)
 {
 	if (!lst_a || !*lst_a || (*lst_a)->next == *lst_a)
 		return ;
@@ -29,9 +41,14 @@ void	swap_a(t_list **lst_a)
 	else
 		do_swap(lst_a);
 	write(1, "sa\n", 3);
+	if (op)
+	{
+		op -> sa++;
+		op -> count++;
+	}
 }
 
-void	swap_b(t_list **lst_b)
+void	swap_b(t_list **lst_b, op_count *op)
 {
 	if (!lst_b || !*lst_b || (*lst_b)->next == *lst_b)
 		return ;
@@ -40,13 +57,18 @@ void	swap_b(t_list **lst_b)
 	else
 		do_swap(lst_b);
 	write(1, "sb\n", 3);
+	if (op)
+	{
+		op->sb++;
+		op->count++;
+	}
 }
 
-void	swap_both(t_list **lst_a, t_list **lst_b)
+void	swap_both(t_list **lst_a, t_list **lst_b, op_count *op)
 {
-	if (lst_a && *lst_a && (*lst_a) -> next != *lst_a)
+	if (lst_a && *lst_a && (*lst_a)->next != *lst_a)
 	{
-		if ((*lst_a) -> next->next == *lst_a)
+		if ((*lst_a)->next->next == *lst_a)
 			*lst_a = (*lst_a)->next;
 		else
 			do_swap(lst_a);
@@ -59,4 +81,9 @@ void	swap_both(t_list **lst_a, t_list **lst_b)
 			do_swap(lst_b);
 	}
 	write(1, "ss\n", 3);
+	if (op)
+	{
+		op->ss++;
+		op->count++;
+	}
 }
