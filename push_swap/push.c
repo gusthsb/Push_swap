22363c99a3f5b8da7445766d3e99527ca025315e
Gustavo Henrique De Souza Brito <gustde-s@c3r11p5.42sp.org.br>
2026-08-10 16:08:03 -0300

    cont op/correct norm

--- push_swap/push.c
+++ push_swap/push.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustde-s <gustde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 16:07:35 by gustde-s          #+#    #+#             */
+/*   Updated: 2026/08/10 16:07:36 by gustde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_push_swap.h"
 
 static void	unlink_top(t_list **src)
@@ -43,14 +55,24 @@ static void	push(t_list **dst, t_list **src)
 	insert_top(dst, moved);
 }
 
-void	push_a(t_list **dst_a, t_list **src_b)
+void	push_a(t_list **dst_a, t_list **src_b, op_count *op)
 {
 	push(dst_a, src_b);
 	write(1, "pa\n", 3);
+	if (op)
+	{
+		op -> pa++;
+		op -> count++;
+	}
 }
 
-void	push_b(t_list **dst_b, t_list **src_a)
+void	push_b(t_list **dst_b, t_list **src_a, op_count *op)
 {
 	push(dst_b, src_a);
 	write(1, "pb\n", 3);
+	if (op)
+	{
+		op -> pa++;
+		op -> count++;
+	}
 }
