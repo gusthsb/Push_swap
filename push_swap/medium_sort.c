84608abef9de99f1f89c74c4c9c1a24e67469d05
Matheus Matos Da Silva Passos <mamatos-@c3r12p4.42sp.org.br>
2026-08-10 19:42:23 -0300

    .

--- push_swap/medium_sort.c
+++ push_swap/medium_sort.c
@@ -6,45 +6,32 @@
 /*   By: mamatos- <mamatos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 18:50:21 by mamatos-          #+#    #+#             */
-/*   Updated: 2026/08/10 18:57:33 by mamatos-         ###   ########.fr       */
+/*   Updated: 2026/08/10 19:41:52 by mamatos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-5
-1
-2
-3
+int	get_chunks(int size)
+{
+	int	chunks;	
 
-1 5
-2
-3
+	chunks = 1;
+	while (chunks * chunks < size)
+		chunks++;
+	return (chunks);
+}
 
-5 1
-2
-3
+void	push_chunk_b(t_list **stack_a, t_list **stack_b,
+	int size, int min_idx)
+{
+	int		i;
+	t_list	*current;
 
-2 5
-3 1
-
-5 2
-3 1
-
-3 5
-  2
-  1
-
-5 3
-  2
-  1
-  
-  5
-  3
-  2
-  1
-
-1
-2
-3 
-5
+	current = stack_a;
+	while (i < size)
+	{
+		...
+		push_b
+	}
+}
