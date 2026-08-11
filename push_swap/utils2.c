031910915b341e362d8b70811be686f1d8911d63
Gustavo Henrique De Souza Brito <gustde-s@c3r7p4.42sp.org.br>
2026-08-11 20:56:28 -0300

    func para inicializar lst

--- push_swap/utils2.c
+++ push_swap/utils2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamatos- <mamatos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustde-s <gustde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 17:08:09 by mamatos-          #+#    #+#             */
-/*   Updated: 2026/08/10 17:46:38 by mamatos-         ###   ########.fr       */
+/*   Updated: 2026/08/11 20:55:41 by gustde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,4 +67,20 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void init_op(t_count *op)
+{
+    op->pa = 0;
+    op->pb = 0;
+    op->ra = 0;
+    op->rb = 0;
+    op->rr = 0;
+    op->sa = 0;
+    op->sb = 0;
+    op->ss = 0;
+    op->rra = 0;
+    op->rrb = 0;
+    op->rrr = 0;
+    op->count = 0;
 }
